dae505904d3f1cb41ae879e96bf91ed3539615b5
Igor <124619600+pett1c@users.noreply.github.com>
2026-03-20 15:19:06 +0100

    Fixed next() async function

--- backend/models/User.js
+++ backend/models/User.js
@@ -33,14 +33,13 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function preSave(next) {
+userSchema.pre('save', async function preSave() {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  return next();
 });
 
 userSchema.methods.comparePassword = async function comparePassword(plainPassword) {
